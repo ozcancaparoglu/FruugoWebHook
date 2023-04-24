ca573ea96300a67717719116f393c094daf18545
ozcan.caparoglu <ozcan.caparoglu@yemeksepeti.com>
2023-04-24 11:06:14 +0300

    order changes

--- Services/Orders/Order.Application/Features/Orders/Commands/GetOrders/GetOrdersCommand.cs
+++ Services/Orders/Order.Application/Features/Orders/Commands/GetOrders/GetOrdersCommand.cs
@@ -5,13 +5,13 @@ namespace Order.Application.Features.Orders.Commands.GetOrders
     public class GetOrdersCommand : IRequest<string>
     {
         public DateTime dateFrom { get; set; }
-        public DateTime dateTo { get; set; }
-        public string orderId { get; set; }
-        public string productId { get; set; }
-        public string skuId { get; set; }
-        public List<string> statuses { get; set; }
-        public List<string> customerCountries { get; set; }
-        public int paymentAmountMin { get; set; }
-        public int paymentAmountMax { get; set; }
+        public DateTime? dateTo { get; set; }
+        public string? orderId { get; set; }
+        public string? productId { get; set; }
+        public string? skuId { get; set; }
+        public List<string>? statuses { get; set; }
+        public List<string>? customerCountries { get; set; }
+        public int? paymentAmountMin { get; set; }
+        public int? paymentAmountMax { get; set; }
     }
 }
